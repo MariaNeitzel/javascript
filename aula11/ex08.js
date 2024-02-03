8ac3bd1df3a0c595cef9c2cf439984100e98592e
MaduNeitzel <eduardaaraujoneitzel@gmail.com>
2024-02-03 13:39:53 -0300

    desafio 1, exercícios 8, 9, 10 e 11

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel= 50
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if(vel > 60) {// Condição simples
+    console.log(`Você ultapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
